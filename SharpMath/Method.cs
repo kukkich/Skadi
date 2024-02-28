3329dbc8e1408c5917b65ca94cbf928976966a27
kukkich <vitia.iezhovqqqq@gmail.com>
2024-02-28 21:42:33 +0700

    Удалил Todo из Method т.к. уже было выполнено

--- SharpMath/Method.cs
+++ SharpMath/Method.cs
@@ -2,9 +2,6 @@
 
 namespace SharpMath;
 
-//TODO Можно добавить поддержку логирования
-// Например, передавать логгер через интерфейс или
-// Использовать Shared логгер, если переданный null
 public abstract class Method<TConfig> 
 {
     protected TConfig Config { get; }
