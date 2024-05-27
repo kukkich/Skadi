13f8a6c653f3f9275f6abff041920d4f1d45b152
kukkich <vitia.iezhovqqqq@gmail.com>
2024-05-27 19:40:48 +0700

    Добавил LU решатель для разреженной через конфертацию в профильный

---
+++ SharpMath/EquationsSystem/Solver/LUSparseThroughProfileConversion.cs
@@ -0,0 +1,17 @@
+﻿using SharpMath.Matrices.Converters;
+using SharpMath.Matrices.Sparse;
+using SharpMath.Vectors;
+
+namespace SharpMath.EquationsSystem.Solver;
+
+public class LUSparseThroughProfileConversion : ISLAESolver<SparseMatrix>
+{
+    public Vector Solve(Equation<SparseMatrix> equation)
+    {
+        var profile = MatrixConverter.Convert(equation.Matrix);
+        var equationProfile = new Equation<ProfileMatrix>(profile, equation.Solution, equation.RightSide);
+        var profileSolver = new LUProfile();
+        
+        return profileSolver.Solve(equationProfile);
+    }
+}
