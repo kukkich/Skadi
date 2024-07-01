095416cc20aa9cf3dd95ad7c1045aca31be28582
kukkich <vitia.iezhovqqqq@gmail.com>
2024-07-02 01:11:57 +0700

    Добавил примитивы

---
+++ SharpMath.Primitives/NonNegative.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace SharpMath.Primitives;
+
+public readonly struct NonNegative<T> where T : INumber<T>
+{
+    public T Value { get; }
+
+    public NonNegative(T value)
+    {
+        if (T.IsPositive(value))
+        {
+            throw new ArgumentException("Value must be non negative", nameof(value));
+        }
+
+        Value = value;
+    }
+
+    public static implicit operator T(NonNegative<T> positive)
+    {
+        return positive.Value;
+    }
+
+    public static implicit operator NonNegative<T>(T value)
+    {
+        return new NonNegative<T>(value);
+    }
+}
