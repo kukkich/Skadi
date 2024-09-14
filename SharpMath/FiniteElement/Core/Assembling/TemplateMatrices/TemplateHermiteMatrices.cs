f8bbef3166073aa93d9a18939e45224a80617f25
zergar5 <93277402+zergar5@users.noreply.github.com>
2024-09-14 20:42:08 +0700

    Added interfaces for splines.

---
+++ SharpMath/FiniteElement/Core/Assembling/TemplateMatrices/TemplateHermiteMatrices.cs
@@ -0,0 +1,14 @@
+﻿using SharpMath.Matrices;
+
+namespace SharpMath.FiniteElement.Core.Assembling.TemplateMatrices;
+
+public static class TemplateHermiteMatrices
+{
+    public static Matrix HermiteStiffness1D(double length) => new Matrix(
+            new double[,]
+            {
+                {1, 1},
+                {1, 1},
+            });
+    
+}
