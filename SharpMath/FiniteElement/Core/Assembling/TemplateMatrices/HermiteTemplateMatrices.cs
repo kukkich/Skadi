fff7a3ab7bc021e9fba2afd24c98b47edbec9aa0
zergar5 <93277402+zergar5@users.noreply.github.com>
2024-09-16 13:01:03 +0700

    Added hermite template matrices.

---
+++ SharpMath/FiniteElement/Core/Assembling/TemplateMatrices/HermiteTemplateMatrices.cs
@@ -0,0 +1,37 @@
+﻿using SharpMath.Matrices;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SharpMath.FiniteElement.Core.Assembling.TemplateMatrices;
+
+public static class HermiteTemplateMatrices
+{
+    public static ImmutableMatrix HermiteStiffness1D(double elementEdgeSize)
+    {
+        return new ImmutableMatrix
+            (
+                new[,]
+                {
+                    {36, 3 * elementEdgeSize, -36, 3 * elementEdgeSize},
+                    {3 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2), -3 * elementEdgeSize, -Math.Pow(elementEdgeSize, 2)},
+                    {-36, -3 * elementEdgeSize, 36, -3 * elementEdgeSize},
+                    {3 * elementEdgeSize, -Math.Pow(elementEdgeSize, 2), -3 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2)},
+                }, 
+                30 * elementEdgeSize
+            );
+    }
+
+    public static ImmutableMatrix HermiteMass1D(double elementEdgeSize)
+    {
+        return new ImmutableMatrix
+            (
+                new[,]
+                {
+                    {156, 22 * elementEdgeSize, 54, -13 * elementEdgeSize},
+                    {22 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2), 13 * elementEdgeSize, -3 * Math.Pow(elementEdgeSize, 2)},
+                    {54, 13 * elementEdgeSize, 156, -22 * elementEdgeSize},
+                    {-13 * elementEdgeSize, -3 * Math.Pow(elementEdgeSize, 2), -22 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2)},
+                },
+                elementEdgeSize / 420
+            );
+    }
+}
