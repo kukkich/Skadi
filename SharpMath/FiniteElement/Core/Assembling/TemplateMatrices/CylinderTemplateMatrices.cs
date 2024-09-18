b1c48bac73e239134abeaa8e7bb6fbd4a53ea3bb
zergar5 <93277402+zergar5@users.noreply.github.com>
2024-09-19 00:21:38 +0700

    Added cylinder template matrices.

---
+++ SharpMath/FiniteElement/Core/Assembling/TemplateMatrices/CylinderTemplateMatrices.cs
@@ -0,0 +1,58 @@
+﻿using SharpMath.Matrices;
+
+namespace SharpMath.FiniteElement.Core.Assembling.TemplateMatrices;
+
+public class CylinderTemplateMatrices
+{
+    public static ImmutableMatrix StiffnessR1D(double r, double elementEdgeSize)
+    {
+        return new ImmutableMatrix
+        (
+            new[,]
+            {
+                { 1d, -1d },
+                { -1d, 1d },
+            },
+            (2 * r + elementEdgeSize) / (2 * elementEdgeSize)
+        );
+    }
+
+    public static ImmutableMatrix StiffnessZ1D(double elementEdgeSize)
+    {
+        return new ImmutableMatrix
+        (
+            new[,]
+            {
+                { 1d, -1d },
+                { -1d, 1d },
+            },
+            1 / elementEdgeSize
+        );
+    }
+
+    public static ImmutableMatrix MassR1D(double r, double elementEdgeSize)
+    {
+        return new ImmutableMatrix
+        (
+            new[,]
+            {
+                {r * 2d + elementEdgeSize / 2, r + elementEdgeSize / 2},
+                {r + elementEdgeSize / 2, r * 2d + elementEdgeSize / 2 * 3},
+            },
+            elementEdgeSize / 6
+        );
+    }
+
+    public static ImmutableMatrix MassZ1D(double elementEdgeSize)
+    {
+        return new ImmutableMatrix
+        (
+            new[,]
+            {
+                { 2d, 1d },
+                { 1d, 2d },
+            },
+            elementEdgeSize / 6
+        );
+    }
+}
--- SharpMath/FiniteElement/Core/Assembling/TemplateMatrices/HermiteTemplateMatrices.cs
+++ SharpMath/FiniteElement/Core/Assembling/TemplateMatrices/HermiteTemplateMatrices.cs
@@ -5,31 +5,31 @@ namespace SharpMath.FiniteElement.Core.Assembling.TemplateMatrices;
 
 public static class HermiteTemplateMatrices
 {
-    public static ImmutableMatrix HermiteStiffness1D(double elementEdgeSize)
+    public static ImmutableMatrix Stiffness1D(double elementEdgeSize)
     {
         return new ImmutableMatrix
             (
                 new[,]
                 {
-                    {36, 3 * elementEdgeSize, -36, 3 * elementEdgeSize},
-                    {3 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2), -3 * elementEdgeSize, -Math.Pow(elementEdgeSize, 2)},
-                    {-36, -3 * elementEdgeSize, 36, -3 * elementEdgeSize},
-                    {3 * elementEdgeSize, -Math.Pow(elementEdgeSize, 2), -3 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2)},
+                    { 36, 3 * elementEdgeSize, -36, 3 * elementEdgeSize },
+                    { 3 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2), -3 * elementEdgeSize, -Math.Pow(elementEdgeSize, 2) },
+                    { -36, -3 * elementEdgeSize, 36, -3 * elementEdgeSize },
+                    { 3 * elementEdgeSize, -Math.Pow(elementEdgeSize, 2), -3 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2) },
                 }, 
                 30 * elementEdgeSize
             );
     }
 
-    public static ImmutableMatrix HermiteMass1D(double elementEdgeSize)
+    public static ImmutableMatrix Mass1D(double elementEdgeSize)
     {
         return new ImmutableMatrix
             (
                 new[,]
                 {
-                    {156, 22 * elementEdgeSize, 54, -13 * elementEdgeSize},
-                    {22 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2), 13 * elementEdgeSize, -3 * Math.Pow(elementEdgeSize, 2)},
-                    {54, 13 * elementEdgeSize, 156, -22 * elementEdgeSize},
-                    {-13 * elementEdgeSize, -3 * Math.Pow(elementEdgeSize, 2), -22 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2)},
+                    { 156, 22 * elementEdgeSize, 54, -13 * elementEdgeSize },
+                    { 22 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2), 13 * elementEdgeSize, -3 * Math.Pow(elementEdgeSize, 2) },
+                    { 54, 13 * elementEdgeSize, 156, -22 * elementEdgeSize },
+                    { -13 * elementEdgeSize, -3 * Math.Pow(elementEdgeSize, 2), -22 * elementEdgeSize, 4 * Math.Pow(elementEdgeSize, 2) },
                 },
                 elementEdgeSize / 420
             );
