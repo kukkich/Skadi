fba4ff369007b09a7431bbca2c868d74ac73e7e6
zergar5 <93277402+zergar5@users.noreply.github.com>
2024-09-18 22:38:18 +0700

    Added smoothing spline creator class.

---
+++ SharpMath/Splines/SmoothingSplineCreator.cs
@@ -0,0 +1,13 @@
+﻿using SharpMath.FiniteElement._2D.Elements;
+using SharpMath.Geometry;
+using SharpMath.Geometry._2D;
+
+namespace SharpMath.Splines;
+
+public class SmoothingSplineCreator : ISplineCreator<Point, BicubicFiniteElement>
+{
+    public ISpline<Point> CreateSpline(Grid<Point, BicubicFiniteElement> grid, FuncValue[] funcValues)
+    {
+        throw new NotImplementedException();
+    }
+}
