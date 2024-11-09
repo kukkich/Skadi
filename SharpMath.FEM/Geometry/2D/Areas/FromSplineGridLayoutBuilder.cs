ce0f95b69ab76410c0843572ba4720538acb6652
kukkich <vitia.iezhovqqqq@gmail.com>
2024-11-09 11:47:32 +0700

    Добавил построение сетки на основе слайна

---
+++ SharpMath.FEM/Geometry/2D/Areas/FromSplineGridLayoutBuilder.cs
@@ -0,0 +1,43 @@
+﻿using SharpMath.FEM.Geometry._2D.Quad;
+using SharpMath.Geometry._2D;
+using SharpMath.Geometry.Shapes;
+using SharpMath.Geometry.Splitting;
+
+namespace SharpMath.FEM.Geometry._2D.Areas;
+
+public class FromSplineGridLayoutBuilder
+{
+    public (Point2D[,] controlPoints, AreaDefinition[] areas) Build(IEnumerable<ICurve<Point2D>> horizontalBounds, ICurveSplitter splitter)
+    {
+        var curveBounds = horizontalBounds.ToArray();
+        var areasCount = (curveBounds.Length - 1) * splitter.Steps;
+        
+        var points = new Point2D[curveBounds.Length, splitter.Steps + 1];
+        var areas = new AreaDefinition[areasCount];
+
+        var areaId = 0;
+        for (var i = 0; i < curveBounds.Length - 1; i++)
+        {
+            for (var j = 0; j < splitter.Steps; j++)
+            {
+                areas[areaId] = new AreaDefinition(j, j + 1, i, i + 1, areaId);
+                areaId++;
+            }
+        }
+
+        var layerIndex = 0;
+        foreach (var bound in curveBounds)
+        {
+            var pointOnLayerIndex = 0;
+            foreach (var point in splitter.EnumerateValues(bound))
+            {
+                points[layerIndex, pointOnLayerIndex] = point;
+                pointOnLayerIndex++;
+            }
+            
+            layerIndex++;
+        }
+        
+        return (points, areas);
+    }
+}
