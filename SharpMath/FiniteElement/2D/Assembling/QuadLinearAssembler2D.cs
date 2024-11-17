d36a659eca4b2812e4bc0a37eb950c722057e532
kukkich <vitia.iezhovqqqq@gmail.com>
2024-11-17 19:34:35 +0700

    Добавил численное интегрирование

---
+++ SharpMath/FiniteElement/2D/Assembling/QuadLinearAssembler2D.cs
@@ -0,0 +1,17 @@
+﻿using SharpMath.FEM.Core;
+using SharpMath.FiniteElement.Core.Assembling;
+
+namespace SharpMath.FiniteElement._2D.Assembling;
+
+public class QuadLinearAssembler2D : ILocalAssembler<IElement>
+{
+    public LocalMatrix AssembleMatrix(IElement element)
+    {
+        throw new NotImplementedException();
+    }
+
+    public LocalVector AssembleRightSide(IElement element)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ SharpMath/Integration/Gauss2D.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Extensions.Logging;
+using SharpMath.Geometry._1D;
+using SharpMath.Geometry._2D;
+
+namespace SharpMath.Integration;
+
+public class Gauss2D : Method<GaussConfig>
+{
+    public Gauss2D(GaussConfig config, ILogger logger)
+        : base(config, logger)
+    {
+        if (config.Segments != 1)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public double Calculate(Func<Point2D, double> f, Line1D xInterval, Line1D yInterval)
+    {
+        var ySum = 0d;
+
+        var xHalfLength = (xInterval.End - xInterval.Start) / 2;
+        var xMid = (xInterval.Start + xInterval.End) / 2;
+        var yHalfLength = (yInterval.End - yInterval.Start) / 2;
+        var yMid = (yInterval.Start + yInterval.End) / 2;
+        
+        for (var i = 0; i < Config.Nodes.Count; i++)
+        {
+            var y = (yHalfLength * Config.Nodes[i] + yMid);
+            var xSum = 0d;
+            for (var j = 0; j < Config.Nodes.Count; j++)
+            {
+                var x = xHalfLength * Config.Nodes[j] + xMid;
+                xSum += Config.Weights[j] * f(new Point2D(x, y));
+            }
+
+            ySum += xHalfLength * xSum;
+        }
+
+        return yHalfLength * ySum;
+    }
+}
+
+public class GaussConfig
+{
+    public IReadOnlyList<double> Nodes { get; init; }
+    public IReadOnlyList<double> Weights { get; init; }
+    public int Segments { get; init; }
+
+    public static GaussConfig Gauss2(int segments) => new()
+    {
+        Nodes = [-1d / Math.Sqrt(3), 1d / Math.Sqrt(3)],
+        Weights = [1d, 1d],
+        Segments = segments
+    };
+    
+    public static GaussConfig Gauss3(int segments) => new()
+    {
+        Nodes = [
+            -Math.Sqrt(3d / 7),
+            0,
+            Math.Sqrt(3d / 7),
+        ],
+        Weights = [
+            5d / 9,
+            8d / 9,
+            5d / 9
+        ],
+        Segments = segments
+    };
+    
+    public static GaussConfig Gauss4(int segments) => new()
+    {
+        Nodes = [
+            -Math.Sqrt((3 - 2 * Math.Sqrt(6d/5)) / 7),
+            Math.Sqrt((3 - 2 * Math.Sqrt(6d/5)) / 7),
+            -Math.Sqrt((3 + 2 * Math.Sqrt(6d/5)) / 7),
+            Math.Sqrt((3 + 2 * Math.Sqrt(6d/5)) / 7),
+        ],
+        Weights = [
+            (18d + Math.Sqrt(30)) / 36,
+            (18d + Math.Sqrt(30)) / 36,
+            (18d - Math.Sqrt(30)) / 36,
+            (18d - Math.Sqrt(30)) / 36,
+        ],
+        Segments = segments
+    };
+}
