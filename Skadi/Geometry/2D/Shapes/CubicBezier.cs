5f2d9fb400903f3ce2f0009c360bfd0a20b61a6b
kukkich <vitia.iezhovqqqq@gmail.com>
2025-01-08 02:22:59 +0700

    Добавил кривую безье и кубический сплайн

---
+++ Skadi/Geometry/2D/Shapes/CubicBezier.cs
@@ -0,0 +1,31 @@
+using Skadi.Geometry.Shapes.Primitives;
+
+namespace Skadi.Geometry._2D.Shapes;
+
+public class CubicBezier : IParametricCurve2D
+{
+    private readonly Vector2D[] _points;
+    public Vector2D Start => _points[0];
+    public Vector2D End => _points[3];
+
+    public CubicBezier(Vector2D[] points)
+    {
+        if (points.Length != 4)
+        {
+            throw new ArgumentException("Invalid number of points");
+        }
+        _points = points;
+    }
+    
+    public Vector2D GetByParameter(CurveParameter t)
+    {
+        Span<double> tc =
+        [
+            Math.Pow(1 - t, 3),
+            3 * t * Math.Pow(1 - t, 2),
+            3 * Math.Pow(t, 2),
+            Math.Pow(t, 3),
+        ];
+        return LinAl.MultiplyAsTransparent<Vector2D>(tc, _points);
+    }
+}
---
+++ Skadi/Geometry/2D/Shapes/CubicSpline.cs
@@ -0,0 +1,31 @@
+using Skadi.Geometry.Shapes.Primitives;
+
+namespace Skadi.Geometry._2D.Shapes;
+
+public class CubicSpline
+{
+    private readonly Vector2D[] _points;
+    public Vector2D Start => _points[0];
+    public Vector2D End => _points[3];
+
+    public CubicSpline(Vector2D[] points)
+    {
+        if (points.Length != 4)
+        {
+            throw new ArgumentException("Invalid number of points");
+        }
+        _points = points;
+    }
+    
+    public Vector2D GetByParameter(CurveParameter t)
+    {
+        Span<double> tc =
+        [
+            -9d / 2 * (t - 1d / 3) * (t - 2d / 3) * (t - 1),
+            27d / 2 * t * (t - 2d / 3) * (t - 1),
+            -27d / 2 * t * (t - 1d / 3) * (t - 1),
+            9d / 2 * t * (t - 1d / 3) * (t - 2d / 3),
+        ];
+        return LinAl.MultiplyAsTransparent<Vector2D>(tc, _points);
+    }
+}
