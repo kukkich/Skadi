7bbf25d7429d68124ce97461fe710716bc67d1c0
Витя <vitia.iezhovqqqq@gmail.com>
2025-09-23 16:56:52 +0300

    Накидал план

---
+++ Skadi/Matrices/Sparse/BlockMatrix.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace Skadi.Matrices;
+
+public class BlockMatrix
+{
+    private readonly int[] _ig;
+    private readonly int[] _jg;
+}
+
+public class ComplexVector
+{
+}
+
+// Скалярное произведение
+// BlockMatrix * ComplexVector
+// предобуславливатель, нужно только: M^-1 * v = ...
