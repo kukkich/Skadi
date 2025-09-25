cf95c4eb39bcf945db5303264a027c86b3c444d9
Витя <vitia.iezhovqqqq@gmail.com>
2025-09-25 20:43:57 +0300

    Удалил устаревший решатель гармонического уравнения

--- Skadi/FEM/Deprecated/Core/Assembling/Boundary/Harmonic/IHarmonicSecondBoundaryApplier.cs
+++
@@ -1,8 +0,0 @@
-﻿using Skadi.EquationsSystem;
-
-namespace Skadi.FEM.Deprecated.Core.Assembling.Boundary.Harmonic;
-
-public interface IHarmonicSecondBoundaryApplier<TMatrix>
-{
-    public void Apply(Equation<TMatrix> equation, HarmonicSecondCondition condition);
-}
