5c0082d0c84f81ae35da872906d6942dc3d26e8d
Витя <vitia.iezhovqqqq@gmail.com>
2025-12-09 17:22:22 +0300

    Создал пустые комплексные решатели

---
+++ Skadi/EquationsSystem/Solver/Complex/ComplexConjugateGradientSolver.cs
@@ -0,0 +1,12 @@
+﻿using Skadi.LinearAlgebra.Matrices.Sparse;
+using Skadi.LinearAlgebra.Vectors;
+
+namespace Skadi.EquationsSystem.Solver.Complex;
+
+public class ComplexConjugateGradientSolver : ISLAESolver<ComplexMatrix>
+{
+    public Vector Solve(Equation<ComplexMatrix> equation)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Skadi/EquationsSystem/Solver/Complex/ComplexLocalOptimalSchemeSolver.cs
@@ -0,0 +1,12 @@
+﻿using Skadi.LinearAlgebra.Matrices.Sparse;
+using Skadi.LinearAlgebra.Vectors;
+
+namespace Skadi.EquationsSystem.Solver.Complex;
+
+public class ComplexLocalOptimalSchemeSolver(CommonIterationSLAESolverConfig config) : IObservableSLAESolver<ComplexMatrix>
+{
+    public Vector Solve(Equation<ComplexMatrix> equation, IProgress<SLAESolverIteration> iteration)
+    {
+        throw new NotImplementedException();
+    }
+}
